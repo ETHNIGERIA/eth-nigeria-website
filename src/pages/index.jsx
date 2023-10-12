f42b6e23eb5f4a20d1746d151083dfd42c86aaab
alabo-excel <iamalaboexcel@gmail.com>
2023-10-12 11:22:58 +0100

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import MainLayout from "@/layout/MainLayout";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <title>Eth Nigeria</title>
+      <MainLayout>
+        hello
+      </MainLayout>
+    </Fragment>
+  )
+}
