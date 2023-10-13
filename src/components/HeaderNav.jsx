b4e5ac7f810c08fb45b335b4038b8ecbce615315
Papi <gj.olaseni@gmail.com>
2023-10-13 05:18:23 +0100

    Blog page

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -37,7 +37,7 @@ const HeaderNav = () => {
             <p className='my-auto text-base'>About Us</p>
           </Link>
 
-          <Link href={'/'} >
+          <Link href={'/blog'} >
             <p className='my-auto text-base'>Blog</p>
           </Link>
         </div>
